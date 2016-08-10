0dae319beb8e7fa066de06de71cb04c8278a80ed
mobalick <mobalick@hotmail.com>
2016-08-10 23:20:07 +0200

    Watching positions and zoom

--- app/pages/map/map.ts
+++ app/pages/map/map.ts
@@ -1,6 +1,8 @@
 import { Component, ViewChild  } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { GOOGLE_MAPS_DIRECTIVES, SebmGoogleMap } from 'angular2-google-maps/core';
+import { Geolocation } from 'ionic-native';
+
 /*
   Generated class for the MapPage page.
 
@@ -15,8 +17,11 @@ export class MapPage {
     
   title: string = 'My first angular2-google-maps project';
   lat: number = 51.678418;
-  lng: number = 7.809007;
+  lng: number = 7.809007; 
+  zoom :number= 18;  
+   watch:any; 
   
+
   @ViewChild(SebmGoogleMap) map: SebmGoogleMap;
   
   constructor(private navCtrl: NavController) {
@@ -26,10 +31,20 @@ export class MapPage {
   ionViewDidEnter() {
     console.log("I'm alive!");
     this.map.triggerResize();
+
+    this.watch = Geolocation.watchPosition().subscribe(pos => {
+      this.lat = pos.coords.latitude;
+      this.lng = pos.coords.longitude;
+
+      console.log('lat: ' + pos.coords.latitude + ', lon: ' + pos.coords.longitude);
+    });
+
   }
   
   ionViewWillLeave() {
     console.log("Looks like I'm about to leave :(");
+
+    this.watch.unsubscribe();
   }
   
   ionViewDidLeave() {
